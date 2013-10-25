9b41a5e220385d36344cd14c788de0c0d4bf1e02
Matt Sayre <mlsayre@gmail.com>
2013-10-25 15:32:32 -0700

    new'd it up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TopPotty::Application.config.secret_token = 'b6ba7a7be387df2d7f8c6b990811ee717faf193bd0c8f8b9bc404b55f00782bafcc426a0979ca9ed159282abde5841364bacce242805a2c6a56944ab75334adc'
