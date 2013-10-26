3f6e89dad6b942d0b0093d97a2dfaf5d63ee4bb0
Matt Sayre <mlsayre@gmail.com>
2013-10-25 17:10:48 -0700

    minitest-rails-capybara installed

--- Rakefile
+++ Rakefile
@@ -5,3 +5,4 @@
 require File.expand_path('../config/application', __FILE__)
 
 TopPotty::Application.load_tasks
+MiniTest::Rails::Testing.default_tasks << "features"
---
+++ test/features/member_contribute_restrooms_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+feature "Member can contribute restrooms" do
+  scenario "Member adds new restroom location" do
+
+    visit root_path
+
+    page.text.wont_include 'Post was successfully created'
+  end
+end
