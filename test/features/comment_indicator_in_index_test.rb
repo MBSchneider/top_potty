6763ee8fc30e798eea39799f05a8913fa26cfb12
Matt Sayre <mlsayre@gmail.com>
2013-11-01 10:19:58 -0700

    comment test

---
+++ test/features/comment_indicator_in_index_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+feature "index shows comments are available to view" do
+  scenario "visitor can see link in index to view comment" do
+
+    sign_in(:one)
+
+    visit new_restroom_path
+
+    fill_in 'Address', with: "123 Oak Rd."
+    choose 'Male'
+    select '10', from: "Cleanliness rating"
+    fill_in 'Comment', with: "This be a comment"
+
+    click_on 'Create Restroom'
+    click_on 'Back'
+
+    page.text.must_include 'See comment'
+
+  end
+end
--- test/features/tpbadge_display_test.rb
+++ test/features/tpbadge_display_test.rb
@@ -2,20 +2,21 @@ require "test_helper"
 
 feature "TPBadge icon marks the best restrooms" do
   scenario "TPBadge picture displays for restrooms with clean rating over 8.8" do
-    pending # image not showing up in capybara - how to test??
+    pending "how to make capybara find image?" # image not showing up in capybara - how to test??
 
     # sign_in(:one)
 
     # visit new_restroom_path
 
-    # fill_in 'Location', with: "123 Oak Rd."
+    # fill_in 'Address', with: "123 Oak Rd."
     # choose 'Male'
     # select '10', from: "Cleanliness rating"
 
     # click_on 'Create Restroom'
     # click_on 'Back'
 
-    # page.body.must_include 'Badge'
+    # save_and_open_page
+    # page.text.must_include 'tpbadge'
 
   end
 end
