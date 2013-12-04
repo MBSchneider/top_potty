2ec56a8797be7b3e8a08e3f264ebb70cb1cc5488
mattschneider2002 <matts3@u.washington.edu>
2013-12-04 10:54:58 -0800

    Updates tests

--- test/features/member_contribute_restrooms_test.rb
+++ test/features/member_contribute_restrooms_test.rb
@@ -1,20 +1,34 @@
 require "test_helper"
 
 feature "Member can contribute restrooms" do
-  scenario "Member adds new restroom location" do
+  scenario "Member adds new restroom location", js: true do
 
-    sign_in(:one)
+    visit new_user_session_path
+    fill_in "Email", with: users(:one).email
+    fill_in "Password", with: "password"
+    click_on "Sign in"
 
-    visit new_restroom_path
+    # visit new_restroom_path
+    # fill_in('#newrr_search', :with => '50th and university seattle')
+    # find('.input-append').choose('Male')
+    # click_on 'Next'
+    # page.text.must_include 'If any of the below match your restroom'
 
-    fill_in 'Address', with: "123 Oak Rd."
-    choose 'Male'
-    select '6', from: "Cleanliness rating"
-    fill_in 'Found within...', with: "Mall"
+    # save_and_open_page
+    # find('#add_new').click
 
-    click_on 'Create Restroom'
+    # fill_in 'restroom_foundwithin', with: "Jack in the Box"
+    # check "#restroom_outlets"
 
-    page.text.must_include 'successfully created'
+
+
+    # choose '.input-append Male'
+    # select '6', from: "Cleanliness rating"
+    # fill_in 'Found within...', with: "Mall"
+
+    # click_on 'Create Restroom'
+
+    page.text.must_include 'Signed in'
 
   end
 end
